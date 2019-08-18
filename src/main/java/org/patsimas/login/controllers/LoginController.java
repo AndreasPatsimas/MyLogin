ca3bd20882bdd9ee92ca4d3af6bb2fa8e31aff02
AndreasPatsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2019-08-18 21:27:51 +0300

    first commit

---
+++ src/main/java/org/patsimas/login/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package org.patsimas.login.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	/*@RequestMapping("/login")
+	public String lognPage() {
+		return "teers.html";
+	}*/
+
+}
+
