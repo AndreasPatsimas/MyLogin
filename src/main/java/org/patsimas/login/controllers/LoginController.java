f413269d8ecccc0dff2cb9bb52f0c5cb68b922cb
AndreasPatsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2019-10-13 11:10:33 +0300

    updates

--- src/main/java/org/patsimas/login/controllers/LoginController.java
+++ src/main/java/org/patsimas/login/controllers/LoginController.java
@@ -1,15 +1,26 @@
 package org.patsimas.login.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LoginController {
 	
-	/*@RequestMapping("/login")
-	public String lognPage() {
-		return "teers.html";
-	}*/
+	@GetMapping("/")
+	public String loginPage() {
+		return ("<h1>Welcome</h1>");
+	}
+	
+	@GetMapping("/user")
+	public String userPage() {
+		return ("<h1>Welcome User</h1>");
+	}
+	
+	@GetMapping("/admin")
+	public String adminPage() {
+		return ("<h1>Welcome Admin</h1>");
+	}
 
 }
 
